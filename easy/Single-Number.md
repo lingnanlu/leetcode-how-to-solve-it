e819655f5dabc2b2db1b6618da5ac1041ef8f1cb
asanelder <lingnanlu@gmail.com>
2020-06-02 15:40:37 +0800

    x

---
+++ easy/Single-Number.md
@@ -0,0 +1,44 @@
+### Single Number
+
+来源:https://leetcode.com/problems/single-number/
+
+问题描述:
+Given a **non-empty** array of integers, every element appears twice except for one. Find that single one.
+
+Note:
+
+Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+Example 1:
+```
+Input: [2,2,1]
+Output: 1
+```
+
+Example 2:
+
+```
+Input: [4,1,2,1,2]
+Output: 4
+```
+
+
+
+解题思路:
+this is the soluction
+
+
+```java
+public class Single_Number {
+
+    public int singleNumber(int[] nums) {
+
+        int result = 0;
+        for (int n : nums) {
+            result = result ^ n;
+        }
+
+        return result;
+    }
+}
+```
--- src/main/java/easy/Single_Number.java
+++ src/main/java/easy/Single_Number.java
@@ -1,5 +1,34 @@
 package easy;
 
+import elder.Question;
+
+@Question(
+        title = "Single Number",
+        url = "https://leetcode.com/problems/single-number/",
+        description = """
+        Given a **non-empty** array of integers, every element appears twice except for one. Find that single one.
+
+        Note:
+
+        Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+        Example 1:
+        ```
+        Input: [2,2,1]
+        Output: 1
+        ```
+
+        Example 2:
+
+        ```
+        Input: [4,1,2,1,2]
+        Output: 4
+        ```
+
+        """,
+        solution = "this is the soluction",
+        category = "easy"
+)
 public class Single_Number {
 
     public int singleNumber(int[] nums) {
